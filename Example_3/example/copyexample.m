762c8f93ea9fcaf91b1654c9752ad9f43092db10
Jiangjiang <zhangjiangjiang.dz@163.com>
2020-03-15 21:04:15 +0800

    Add files via upload

--- Example_3/example/copyexample.m
+++ Example_3/example/copyexample.m
@@ -1,1 +1,10 @@
-
+function []= copyexample(NumPoints,flag)
+if nargin < 2  
+    parfor i=2:NumPoints
+        copyfile('parallel_1',['parallel_',num2str(i)]);
+    end    
+elseif flag == -1
+    parfor i = 2:NumPoints
+        rmdir(['parallel_',num2str(i)],'s');
+    end
+end
